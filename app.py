5f9c82096e27b19a109f60734e1f3ac0871c6a08
yoxdele <82913471+yoxdele@users.noreply.github.com>
2025-11-13 21:49:55 +0000

    Change home route response message
    Updated the home route response message.

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "🚀 Flask App Deployed via GitHub Actions & AWS!"
+    return "Flask App Deployed"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
+
